f8d07fb478ab8758eb0343680b30dd5970e08691
Jorge Aguirre <jorge-l-am@hotmail.com>
2023-03-14 20:13:27 -0600

    Add preview of image uploaded

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
             // Specifies the source and destination file path to move the source file to the target as specified
             if(move_uploaded_file($_FILES['myfile']['tmp_name'],"$targetDir/".$_FILES['myfile']['name'])) {
                 echo "File uploaded successfully!";
+                $newDir = "$targetDir/".$_FILES['myfile']['name'];
+                // Show a preview of image uploaded
+                echo '<img src="'.$newDir.'">';
             } else {
                 echo "Sorry, there was an error uploading your file.";
             }
